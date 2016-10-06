b24c25337ca05eee7d5d250d5c73ebd4ee7db3b4
Shannon McMurtrey <shannon@cart32.com>
2016-10-06 11:46:27 -0500

    fixing layout

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -3,6 +3,7 @@ class Individual < ApplicationRecord
   enum ethnicity: { "White" => 0, "Hispanic/Latino" => 1, "Black or African American" =>  2, "Asian" => 3, "American Native or Alaska Native" => 4, "Middle Eastern or North African" => 5, "Native Hawaiian or Pacific Islander" => 6, "Other race, ethnicity, or origin" => 7 }
   enum springfield_non_native_population: {"Less than 2,000" => 0, "2,000-9,999" => 1, "10,000-49,000"=>2, "50,000-99,000"=>3, "100,000-249,000"=>4, "250,000 to 1 million"=>5, "Over 1 million"=>6}
   enum springfield_non_native_distance: {"under 25 miles"=>0, "26-50 miles"=>1, "51-100 miles"=>2, "over 100 miles"=>3}
+  enum childhood_raisedby_other_than_parents_by_who: {"Grandparent(s)"=>0, "Aunt/Uncle"=>1, "Other relative"=>2, "Not a relative"=>3}
 
   def self.search(search)
     where("first_name || last_name LIKE ?", "%#{search}%")
