0344367b93e75d4d45da853b868158b6ee509a6c
Shannon McMurtrey <shannon@cart32.com>
2016-10-07 10:18:10 -0500

    Fixing forms

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -17,6 +17,7 @@ class IndividualsController < ApplicationController
   # GET /individuals/1
   # GET /individuals/1.json
   def show
+    @cohort = Cohort.find(@individual.cohort_id)
   end
 
   # GET /individuals/new
@@ -27,6 +28,7 @@ class IndividualsController < ApplicationController
 
   # GET /individuals/1/edit
   def edit
+    @cohort = Cohort.find(@individual.cohort_id)
   end
 
   # POST /individuals
@@ -77,7 +79,7 @@ class IndividualsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def individual_params
-      params.require(:individual).permit(:first_name, :last_name, :address_line_1, :address_line_2, :city, :state, :zip, :phone_number, :mobile_phone_number, :email_address, :DOB, :start_date, :us_citizen, :non_us_citizen, :ethnicity, :ethnicity_other, :lanuage_primary, :language_other, :springfield_native, :springfield_native_neighborhood, :springfield_non_native_population, :neighborhood_resident_length_in_months, :springfield_non_native_distance, :cohort_id)
+      params.require(:individual).permit(:first_name, :last_name, :address_line_1, :address_line_2, :city, :state, :zip, :phone_number, :mobile_phone_number, :email_address, :DOB, :start_date, :us_citizen, :non_us_citizen, :ethnicity, :ethnicity_other, :lanuage_primary, :language_other, :springfield_native, :springfield_native_neighborhood, :springfield_non_native_population, :neighborhood_resident_length_in_months, :springfield_non_native_distance, :childhood_raisedby_other_than_parents, :childhood_raisedby_other_than_parents_length_in_months, :childhood_raisedby_other_than_parents_by_who, :childhood_residence_away_from_family, :childhood_residence_away_from_family_length_in_months, :childhood_swore_at_or_put_down, :childhood_made_to_feel_afraid_of_injury, :childhood_pushed_shoved_grabbed_slapped, :childhood_hit_hard, :childhood_lived_with_alcoholic, :childhood_lived_with_drug_user, :childhood_lived_with_depressed_or_mentally_ill, :childhood_lived_with_member_who_attempted_suicide, :childhood_lived_with_member_went_to_jail, :childhood_jailed_family_member_which, :childhood_caregiver_was_pushed_shoved_grabbed_slapped, :childhood_caregiver_bitten_or_hit, :childhood_caregiver_hit_repeatedly, :childhood_caregiver_threatened_with_weapon, :cohort_id)
     end
     def verify_is_admin
       (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
