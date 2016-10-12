8df74c5ff90f9c8d52b125c9b6181118cf3b8836
Shannon McMurtrey <shannon@cart32.com>
2016-10-12 12:53:05 -0500

    added attendance feature

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,7 @@
 class AttendancesController < ApplicationController
     before_action :set_attendance, only: [:show, :edit, :update, :destroy]
 
+
   def index
     @attendances = Attendance.all
   end
@@ -8,29 +9,48 @@ class AttendancesController < ApplicationController
   def show
   end
 
-  def new
-    @meeting = Meeting.find(params[:meeting_id])
-    @attendees = @meeting.cohort.individuals
-    @attendance = Attendance.new(meeting_id: @meeting.id)
+  def edit
+    
   end
 
-  def create
-    @attendance = Attendance.new(attendance_params)
-
+  def update
     respond_to do |format|
-      if @attendance.save
-        format.html { redirect_to @attendance, notice: 'Attendance was successfully created.' }
-        format.json { render :show, status: :created, location: @attendance }
+      if @attendance.update(edit_attendance_params)
+        format.html { redirect_to @attendance, notice: 'Attendance was successfully updated.' }
+        format.json { render :show, status: :ok, location: @attendance }
       else
-        format.html { render :new }
+        format.html { render :edit }
         format.json { render json: @attendance.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  def new
+    @attendances = []
+    @meeting = Meeting.find(params[:meeting_id])
+    @meeting.cohort.individual_ids.map do |individual_id|
+      @attendances << Attendance.new(individual_id: individual_id, meeting_id: @meeting.id)
+    end
+  end
+
+  def create
+    params["attendance"].each do |attendance|
+      @attendance = Attendance.create(attendance_params(attendance))
+    end
+
+    respond_to do |format|
+      if @attendance.save
+        format.html {redirect_to meetings_path, notice: "You added the attendance!" }
+      else
+        redirect_to new_attendances_path(attendance_params)
+      end
+   end
+  end
+
   
 
   def destroy
+    redirect_to attendances_path
   end
 
   private
@@ -40,7 +60,13 @@ class AttendancesController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def attendance_params
-      params.require(:attendance).permit(:individual_id, :meeting_id)
+    def attendance_params(attendance)
+      params.require(:attendance).permit!
+      return params["attendance"][attendance]
     end
+
+    def edit_attendance_params
+      params.require(:attendance).permit!
+    end
+
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,5 @@
 class Meeting < ApplicationRecord
   belongs_to :cohort
+  has_many :attendances
   has_and_belongs_to_many :individuals
 end
