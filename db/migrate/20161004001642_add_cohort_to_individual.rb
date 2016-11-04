20f95e8f1a0cad05496ff271dd2f07a769893f4b
Shannon McMurtrey <shannon@cart32.com>
2016-11-03 21:17:37 -0500

    fixing migration:

--- db/migrate/20161004001642_add_cohort_to_individual.rb
+++ db/migrate/20161004001642_add_cohort_to_individual.rb
@@ -1,5 +1,5 @@
 class AddCohortToIndividual < ActiveRecord::Migration[5.0]
   def change
-    add_reference :individuals, :cohort, foreign_key: true
+    #add_reference :individuals, :cohort, foreign_key: true
   end
 end
