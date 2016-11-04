9cdbed8de154fceccc12a2df547a126640159e54
Shannon McMurtrey <shannon@cart32.com>
2016-11-03 23:17:57 -0500

    adding field

--- db/migrate/20161018191110_add_screening_fields_to_individual.rb
+++ db/migrate/20161018191110_add_screening_fields_to_individual.rb
@@ -68,6 +68,7 @@ class AddScreeningFieldsToIndividual < ActiveRecord::Migration[5.0]
     add_column :individuals, :nwp_currently_eligible, :boolean
     add_column :individuals, :square_footage_of_house, :float
     add_column :individuals, :notes, :text
+    add_column :individuals, :preferred_method_of_contact, :text
 
   end
 end
