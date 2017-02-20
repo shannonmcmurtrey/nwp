c1fb362fabe5e86b1e70bfeb84f8572ea6ae2c48
Shannon McMurtrey <shannon@cart32.com>
2017-02-20 10:29:47 -0600

    started work on goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -15,21 +15,25 @@ class GoalsController < ApplicationController
 
   # GET /goals/new
   def new
+    @individual = Individual.find(params[:individual_id])
     @goal = Goal.new
   end
 
   # GET /goals/1/edit
   def edit
+    @individual = @goal.individual
   end
 
   # POST /goals
   # POST /goals.json
   def create
     @goal = Goal.new(goal_params)
+    @individual = @goal.individual
+
 
     respond_to do |format|
       if @goal.save
-        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
+        format.html { redirect_to @individual, notice: 'Goal was successfully created.' }
         format.json { render :show, status: :created, location: @goal }
       else
         format.html { render :new }
@@ -41,9 +45,10 @@ class GoalsController < ApplicationController
   # PATCH/PUT /goals/1
   # PATCH/PUT /goals/1.json
   def update
+    @individual = @goal.individual
     respond_to do |format|
       if @goal.update(goal_params)
-        format.html { redirect_to @goal, notice: 'Goal was successfully updated.' }
+        format.html { redirect_to @individual, notice: 'Goal was successfully updated.' }
         format.json { render :show, status: :ok, location: @goal }
       else
         format.html { render :edit }
