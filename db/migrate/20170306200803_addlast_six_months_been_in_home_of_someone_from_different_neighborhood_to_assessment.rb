237dc6b3bc68358571c9b080e29b966b6a9af2b0
Shannon McMurtrey <shannon@cart32.com>
2017-03-06 14:36:16 -0600

    bug fix

--- db/migrate/20170306200803_addlast_six_months_been_in_home_of_someone_from_different_neighborhood_to_assessment.rb
+++
@@ -1,5 +0,0 @@
-class AddlastSixMonthsBeenInHomeOfSomeoneFromDifferentNeighborhoodToAssessment < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :assessments, :last_six_months_been_in_home_of_someone_from_different_neighborhood, :boolean
-  end
-end
