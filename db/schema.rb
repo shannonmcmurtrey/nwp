0408befc1bd916e622d8a91f56b04244ff0e6944
Shannon McMurtrey <shannon@cart32.com>
2017-03-06 14:26:16 -0600

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220162607) do
+ActiveRecord::Schema.define(version: 20170306200803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
