3c409c204940fb73e60d582047ee6e123f733183
Shannon McMurtrey <shannonmcmurtrey@wpad.local>
2017-11-04 17:45:15 -0500

    add super user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104213232) do
+ActiveRecord::Schema.define(version: 20171104223732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -482,6 +482,7 @@ ActiveRecord::Schema.define(version: 20171104213232) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "social_worker",          default: false
+    t.boolean  "super_user",             default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["organization_id"], name: "index_users_on_organization_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
