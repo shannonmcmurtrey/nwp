d04fad0d4decc3b4d0c307170b7b5ddbf6acdfaf
Shannon McMurtrey <shannonmcmurtrey@wpad.local>
2018-04-27 14:19:14 -0500

    adding fields to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(first_name:  "Shannon",
 	         last_name:   "McMurtrey",
+	         admin:       true,
+	         super_user:  true,
+	         social_worker: true,
              email: "smcmurtrey@drury.edu",
              password:              "password")
 
 User.create!(first_name:  "Eric",
 	         last_name:   "McCoullough",
+	         admin:       true,
+	         super_user:  true,
+	         social_worker: true,
              email: "eric96@live.missouristate.edu",
              password:              "password")
