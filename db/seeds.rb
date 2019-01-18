8a0e9e3eb9d744d2d6630887d2d995f6400ca1c7
Jason Klein <jason.klein@logicforte.com>
2019-01-18 11:27:42 -0600

    Update database seeds formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(first_name:  "Shannon",
-	         last_name:   "McMurtrey",
-	         admin:       true,
-	         super_user:  true,
-	         social_worker: true,
-             email: "smcmurtrey@drury.edu",
-             password:              "password")
+User.create(
+  first_name: "Shannon",
+  last_name: "McMurtrey",
+  admin: true,
+  super_user: true,
+  social_worker: true,
+  email: "smcmurtrey@drury.edu",
+  password: "password"
+)
 
-User.create!(first_name:  "Eric",
-	         last_name:   "McCoullough",
-	         admin:       true,
-	         super_user:  true,
-	         social_worker: true,
-             email: "eric96@live.missouristate.edu",
-             password:              "password")
+User.create(
+  first_name: "Eric",
+  last_name: "McCoullough",
+  admin: true,
+  super_user: true,
+  social_worker: true,
+  email: "eric96@live.missouristate.edu",
+  password: "password"
+)
 
-Organization.create!(name: "The Fairbanks", 
-	                 address_line_1: "washington", 
-	                 address_line_2: "", 
-	                 city: "Springfield", 
-	                 state: "MO", 
-	                 zip: "65802", 
-	                 phone_number: nil, 
-	                 website: "", 
-	                 primary_contact: "", 
-	                 code: "nwp")
+Organization.create(
+  name: "The Fairbanks",
+  address_line_1: "washington",
+  address_line_2: "",
+  city: "Springfield",
+  state: "MO",
+  zip: "65802",
+  phone_number: nil,
+  website: "",
+  primary_contact: "",
+  code: "nwp"
+)
